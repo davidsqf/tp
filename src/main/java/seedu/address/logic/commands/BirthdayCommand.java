e17cf14cdbfd7a3e6b92ca3bd7707705b9f98159
davidsqf <davidsongqifeng@gmail.com>
2020-09-28 14:03:19 +0800

    reorder imports in lexicographical order

--- src/main/java/seedu/address/logic/commands/BirthdayCommand.java
+++ src/main/java/seedu/address/logic/commands/BirthdayCommand.java
@@ -8,8 +8,9 @@ import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-import seedu.address.model.person.Person;
 import seedu.address.model.person.Birthday;
+import seedu.address.model.person.Person;
+
 
 
 
