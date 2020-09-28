d45d12c1bb90264725c69948f2455ee1fede2eef
davidsqf <davidsongqifeng@gmail.com>
2020-09-28 12:42:21 +0800

    modify AddressBookParser.java

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -15,6 +15,7 @@ import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.BirthdayCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
@@ -67,7 +68,10 @@ public class AddressBookParser {
 
         case HelpCommand.COMMAND_WORD:
             return new HelpCommand();
-
+        
+        case BirthdayCommand.COMMAND_WORD:
+            return new BirthdayCommandParser().parse(arguments);
+        
         default:
             throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
         }
